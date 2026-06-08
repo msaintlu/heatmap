e8c74659eeccb8b80842ebeb908685700d9a569d
Marion Saint-Lu <25301295+msaintlu@users.noreply.github.com>
2026-06-08 15:27:01 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p
+          style={{
+            fontWeight: "bolder",
+            fontSize: 26,
+            marginLeft: 50,
+            marginTop: 50,
+            marginBottom: 20,
+          }}
+        >
+          Temperature heatmap
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App
